c3cbc90d320275f31eb5545c17c2d588f25b08b5
Isaias <isaiascabrera20@gmail.com>
2025-04-05 15:59:40 -0300

    Se comienza a desplegar el servidor web

---
+++ Source/app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000
+
+app.use(express.static("Source"));
+
+/*
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/HTML/home.html")
+});
+*/
+app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:${PORT}`));
