7918ad3f41e56934a1d9fcb26f6779e50d13cf6d
Isaias <isaiascabrera20@gmail.com>
2025-06-02 14:58:42 -0300

    Cambios en configuraciones del proyecto

--- src/config/connections.js
+++ src/config/connections.js
@@ -1,19 +1,18 @@
 
-const postgres = require('pg');
+const { Pool } = require('pg');
 
-const pool = postgres.create({
+const pool = new Pool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    max: process.env.DB_CONNECTION_LIMIT,
 });
 
 pool.connect((error, client, release) => {
     if (error) {
-        console.error('No se pudo establecer una conexión', error.stack);
+        console.error('No se pudo establecer una conexión -', error.stack);
     } else {
         console.log('Conexión exitosa a la Base de Datos PostgreSQL');
         release();
